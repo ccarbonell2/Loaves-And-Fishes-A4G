c46887e083f1ccebc852444eaa822e8cba327198
[cecilia] <[cecilia.l.carbonell@gmail.com]>
2024-04-02 09:55:57 -0400

    updated preferences

---
+++ src/Components/Preferences.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Preferences() {
+    return (
+        <div>
+            <p>Preferences</p>
+            
+        </div>
+    );
+};
+
+export default Preferences;
