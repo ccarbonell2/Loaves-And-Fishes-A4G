980bcd3436eda0c6e52a34bdc58efebd567c91e0
tschowalter <tjschowalter@wpi.edu>
2024-04-26 09:21:34 -0400

    trying to troubleshoot issues

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Components/Images/Logo.png';
 import './App.css';
-import HelloWorld from './Components/HelloWorld';
-import CounterExample from './Components/CounterExample';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import Logo from "./Components/Images/Logo.png";
 
 function App() {
   return (
     <div >
       <Header />
-      <img src={Logo.png} alt="Logo"></img>
+      <logo />
       <h1>Welcome to our website!</h1>
       <h2>
         Please tell us a bit about your preferences if this is your first time visiting or schedule a time to pick up your food.
